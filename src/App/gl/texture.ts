8fcaf03d656bc10a1fb0f73ddf58947fa6772b9c
Eric-Schecter <eric199002@icloud.com>
2023-01-14 20:55:56 +0800

    add default valut to texture and remove used code

--- src/App/gl/texture.ts
+++ src/App/gl/texture.ts
@@ -2,51 +2,25 @@ export class Texture {
   public id: WebGLTexture = 0;
   public location: WebGLUniformLocation = 0;
   constructor(private gl: WebGL2RenderingContext) { }
-  private setup = (width: number, height: number, data: ArrayBufferView) => {
-    // create + bind
+  private setupImage = (image: HTMLImageElement,internalFormat:number,format:number,type:number,wrapS:number, wrapT:number, minFilter:number, magFilter:number) => {
     this.id = this.gl.createTexture() as WebGLTexture;
     this.gl.bindTexture(this.gl.TEXTURE_2D, this.id);
-    // set parameters
-    this.gl.pixelStorei(this.gl.UNPACK_FLIP_Y_WEBGL, true);
-    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.REPEAT);
-    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.REPEAT);
-    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR);
-    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR);
-    // set data
-    this.gl.texStorage2D(this.gl.TEXTURE_2D, 1, this.gl.RGBA8, width, height);
-    this.gl.texSubImage2D(this.gl.TEXTURE_2D, 0, 0, 0, width, height, this.gl.RGBA, this.gl.UNSIGNED_BYTE, data);
-    // mipmap
+    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, wrapS);
+    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, wrapT);
+    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, minFilter);
+    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, magFilter);
+    this.gl.texImage2D(this.gl.TEXTURE_2D, 0, internalFormat,format, type, image);
     this.gl.generateMipmap(this.gl.TEXTURE_2D);
   }
-  private setupImage = (image: HTMLImageElement) => {
-    // create + bind
-    this.id = this.gl.createTexture() as WebGLTexture;
-    this.gl.bindTexture(this.gl.TEXTURE_2D, this.id);
-    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.REPEAT);
-    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.REPEAT);
-    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR);
-    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR);
-    this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA,this.gl.RGBA, this.gl.UNSIGNED_BYTE, image);
-    this.gl.generateMipmap(this.gl.TEXTURE_2D);
-  }
-  public load = (path: string) => {
+  public load = (path: string,internalFormat=this.gl.RGBA,format=this.gl.RGBA,type=this.gl.UNSIGNED_BYTE,
+    wrapS=this.gl.REPEAT, wrapT=this.gl.REPEAT, minFilter=this.gl.LINEAR, magFilter=this.gl.LINEAR) => {
     return new Promise((resolve, reject) => {
         const image = new Image();
         image.src = path;
         image.onload = ()=>{
-          this.setupImage(image);
+          this.setupImage(image,internalFormat,format,type,wrapS, wrapT, minFilter, magFilter);
           resolve(true);
         }
-      // fetch(path)
-      //   .then(res => res.arrayBuffer())
-      //   .then(buffer => {
-      //     this.setup(128, 128, new Uint8Array(buffer));
-      //     resolve(true);
-      //   })
-      //   .catch((err) => {
-      //     console.log(err);
-      //     reject(false);
-      //   });
     })
   }
 }
